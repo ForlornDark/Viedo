10ba54b32b67b4a7fc9cd7feaa84fbcf37babe5e
lfm1107015 <lfm@localhost.localdomain>
2017-05-11 18:23:18 +0800

    添加常量

---
+++ src/main/java/com/llhc/video/util/RoleConst.java
@@ -0,0 +1,5 @@
+package com.llhc.video.util;
+
+public class RoleConst {
+	public static enum ROLE{USER,MANAGER}
+}
---
+++ src/main/java/com/llhc/video/util/VideoConst.java
@@ -0,0 +1,10 @@
+package com.llhc.video.util;
+
+public class VideoConst {
+	//电影类型枚举
+	public static enum TYPE {ACTION,COMEDY,LOVE,SCIENCE,DISASTER,TERROR,CARTOON,FANTASY,WAR,OTHER};
+	//电影地区枚举
+	public static enum AREA{CN,TW,KR,JP,US,OTHER};
+	//电影语言
+	public static enum LANGUAGE{ZH,EN,KO,JA,OTHER};
+}
