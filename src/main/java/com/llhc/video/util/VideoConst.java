9bd4e318f42854b5001d5b586c3d57f9f87e4919
lfm1107015 <lfm@localhost.localdomain>
2017-05-21 10:15:47 +0800

    添加页面简单的分类布局，修改VideoConst中的常量

--- src/main/java/com/llhc/video/util/VideoConst.java
+++ src/main/java/com/llhc/video/util/VideoConst.java
@@ -2,9 +2,9 @@ package com.llhc.video.util;
 
 public class VideoConst {
 	//电影类型枚举
+	//动作，喜剧，爱情，科幻，灾难，惊悚，动画，奇幻，战争，其他
 	public static enum TYPE {ACTION,COMEDY,LOVE,SCIENCE,DISASTER,TERROR,CARTOON,FANTASY,WAR,OTHER};
 	//电影地区枚举
-	public static enum AREA{CN,TW,KR,JP,US,OTHER};
-	//电影语言
-	public static enum LANGUAGE{ZH,EN,KO,JA,OTHER};
+	//大陆，香港，美国,韩国，日本，台湾，法国，英国,其他
+	public static enum AREA{China,HongKong,UniteStates,Korea,Japan,Taiwan,France,GreatBritain,Other};
 }
