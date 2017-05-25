56c070181ab6d6974f1fe8e63b9677e288306842
lfm1107015 <lfm@localhost.localdomain>
2017-05-25 19:36:12 +0800

    添加评分插件

---
+++ src/main/webapp/js/detail.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+function getScore(){
+	
+	
+}
+ function copyToClipBoard2() {
+s = document.getElementsByClassName("url_text")[0];
+s.select(); // 选择对象
+document.execCommand("Copy"); // 执行浏览器复制命令
+alert("已复制好，可贴粘。");
+ 
+ }
+ 
+            
---
+++ src/main/webapp/js/score.js
@@ -0,0 +1,36 @@
+$(function() {
+    function ratingEnable() {
+       
+
+        $('#example-f').barrating({
+            wrapperClass: 'br-wrapper-f',
+            showSelectedRating: true,
+        });
+
+ 
+    }
+
+    function ratingDisable() {
+        $('select').barrating('destroy');
+    }
+
+    $('.rating-enable').click(function(event) {
+        event.preventDefault();
+
+        ratingEnable();
+
+        $(this).addClass('deactivated');
+        $('.rating-disable').removeClass('deactivated');
+    });
+
+    $('.rating-disable').click(function(event) {
+        event.preventDefault();
+
+        ratingDisable();
+
+        $(this).addClass('deactivated');
+        $('.rating-enable').removeClass('deactivated');
+    });
+
+    ratingEnable();
+});
